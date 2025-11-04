52366afd07b575c575b9ba22f4dd36303dca4baf
Samuel <samuel21@connect.hku.hk>
2025-11-04 11:21:19 +0800

    editeed evaluate.py

--- src/evaluate.py
+++ src/evaluate.py
@@ -1,5 +1,16 @@
 #define metrics like R², RMSE, MAE
+import numpy as np
+from sklearn.metrics import mean_absolute_error, mean_squared_error, mean_squared_log_error, r2_score
 
+def evaluate_model(model, X_test, y_test):
+    y_predicted = model.predict(X_test)
+    y_error = y_test - y_error
 
-def evaluate_model(model, X, y):
-    pass
+    Q1 = np.percentile(np.abs(y_error), 25)
+    MedAE = np.percentile(np.abs(y_error), 50)
+    Q3 = np.percentile(np.abs(y_error), 75)
+
+    MAE = mean_absolute_error(y_test, y_predicted)
+    RMSE = np.sqrt(mean_squared_error(y_test, y_predicted))
+    MSLE = mean_squared_log_error(y_test, y_predicted)
+    R2 = r2_score(y_test, y_predicted)
--- src/train_model.py
+++ src/train_model.py
@@ -3,18 +3,21 @@ from sklearn.model_selection import GridSearchCV, train_test_split
 from sklearn.metrics import make_scorer, r2_score
 
 
-def train_random_forest(X,y):
+def train_random_forest(X,y,define_parameter=None):
     #Step 1: Partition
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25, random_state=42)
 
     #Step 2: Define the grids
-    param_grid = {
-        'n_estimators': [500, 1000, 2500, 3000],
-        'max_depth': [8, 16, 32, 64],
-        'bootstrap': [True, False]
-    }
+    if define_parameter is None:
+        param_grid = {
+            'n_estimators': [500, 1000, 2500, 3000],
+            'max_depth': [8, 16, 32, 64],
+            'bootstrap': [True, False]
+        }
+    else:
+        param_grid = define_parameter
 
-    #Step 3: Set up grid search and fir
+    #Step 3: Set up grid search and fit
     regressor = RandomForestRegressor(random_state=42, n_jobs=-1)
     grid_search = GridSearchCV(
         estimator=regressor,
@@ -28,8 +31,8 @@ def train_random_forest(X,y):
     #Step 4: Find the best model 
     best_model = grid_search.best_estimator_
     best_params = grid_search.best_params_
-    test_s = r2_score(y_test, best_model.predict(X_test))
+    #test_s = r2_score(y_test, best_model.predict(X_test))
 
     #According to the paper, n_estimators = 2500, max_depth = 32, bootstrap = F
 
-    return best_model, best_params, test_s
+    return best_model, best_params, X_test, y_test
