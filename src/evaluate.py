aaad882a3a40a2faa570f3f6cf6e02917e6427be
Samuel <samuel21@connect.hku.hk>
2025-11-04 12:58:00 +0800

    updated evaluate.py

--- src/evaluate.py
+++ src/evaluate.py
@@ -6,11 +6,14 @@ def evaluate_model(model, X_test, y_test):
     y_predicted = model.predict(X_test)
     y_error = y_test - y_error
 
-    Q1 = np.percentile(np.abs(y_error), 25)
-    MedAE = np.percentile(np.abs(y_error), 50)
-    Q3 = np.percentile(np.abs(y_error), 75)
+    metrics = dict()
+    metrics['Q1'] = np.percentile(np.abs(y_error), 25)
+    metrics['MedAE'] = np.percentile(np.abs(y_error), 50)
+    metrics['Q3'] = np.percentile(np.abs(y_error), 75)
 
-    MAE = mean_absolute_error(y_test, y_predicted)
-    RMSE = np.sqrt(mean_squared_error(y_test, y_predicted))
-    MSLE = mean_squared_log_error(y_test, y_predicted)
-    R2 = r2_score(y_test, y_predicted)
+    metrics['MAE'] = mean_absolute_error(y_test, y_predicted)
+    metircs['RMSE'] = np.sqrt(mean_squared_error(y_test, y_predicted))
+    metrics['MSLE'] = mean_squared_log_error(y_test, y_predicted)
+    metrics['R2'] = r2_score(y_test, y_predicted)
+
+    
