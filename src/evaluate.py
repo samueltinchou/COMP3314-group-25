4c32c3cdea92f5ae93b88006de65f422e15daef0
Samuel <samuel21@connect.hku.hk>
2025-11-04 15:33:06 +0800

    added shap analysis

--- src/evaluate.py
+++ src/evaluate.py
@@ -1,6 +1,8 @@
 #define metrics like R², RMSE, MAE
 import numpy as np
 from sklearn.metrics import mean_absolute_error, mean_squared_error, mean_squared_log_error, r2_score
+import shap
+
 
 def evaluate_model(model, X_test, y_test):
     y_predicted = model.predict(X_test)
@@ -17,4 +19,30 @@ def evaluate_model(model, X_test, y_test):
     metrics['R2'] = r2_score(y_test, y_predicted)
 
     return metrics
-    
+
+def shap_initialise(model, X, n):
+    sample_index = np.random.choice(len(X), size = n, replace = False)
+    X_sample = X.iloc[sample_index]
+    explain = shap.TreeExplainer(model)
+    shap_values = explain.shap_values(X_sample)
+    return explain, shap_values, X_sample
+
+def compute_shap_local(model, X_test, n = 100):
+    explain, shap_values, X_sample = shap_initialise(model, X_test, n)
+
+    #Local
+    i = 0 #Random instance
+    shap.force_plot(
+        explain.expected_value,
+        shap_values[i],
+        X_sample.iloc[i],
+        matplotlib = True
+    )
+    shap.plots.bar(shap_values[i])
+
+def compute_shap_global(model, X_test, n = 100):
+    explain, shap_values, X_sample = shap_initialise(model, X_test, n)
+
+    #Global
+    shap.summary_plot(shap_values, X_sample)
+    shap.summary_plot(shap_values, X_sample, plot_type="dot")
